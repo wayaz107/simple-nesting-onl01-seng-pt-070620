00b8ed5c855b3336473833da0b84ebb5d09d969f
Warda Ayaz <wayaz107@gmail.com>
2020-07-18 22:08:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie]
+		 programmer_hash[:dennis_ritchie][:known_for]
 end
 
 def adding_matz
