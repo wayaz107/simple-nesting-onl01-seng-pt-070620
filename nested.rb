4e3f5cb7892229bb7d421628b214e7275e55fb8e
Warda Ayaz <wayaz107@gmail.com>
2020-07-18 22:29:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto]= :known_for ,:languages
-programmer_hash[:yukihiro_matsumoto][:unknown_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages]=["LISP","C"]
+programmer_hash[:yukihiro_matsumoto]= {known_for: "Ruby", languages: ["LISP","C"]}
+
+programmer_hash
 
 end
 # add the following information to the top level of programmer_hash
